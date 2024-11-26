89553eb7d9d5c90422b8907d143fa09db39092fa
Jheno Syechlo <jhenodahlia@gmail.com>
2024-11-26 23:08:00 +0700

    data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 My new app is streamlit")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.title("Upload and Display datasetWhiteMerged.csv")
+
+uploaded_file = st.file_uploader("Choose the datasetWhiteMerged.csv file", type="csv")
+
+if uploaded_file is not None:
+    # Read the CSV file
+    df = pd.read_csv(uploaded_file)
+    st.write("Uploaded CSV File:")
+    st.dataframe(df)
